a5fdca959ce7bb55b3cc2e7bb053184761fa4cca
Mishardina <mizsazms@gmail.com>
2022-11-01 16:12:33 +0300

    Add some solutions with single-linked lists

---
+++ odd-even-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:     
+        if not head:
+            return None
+        
+        odd_end = head
+        
+        even_end = head.next
+        even_head = even_end
+        
+        while even_end and even_end.next:
+            odd_end.next = even_end.next
+            odd_end = odd_end.next
+            even_end.next = odd_end.next
+            even_end = even_end.next
+        
+        odd_end.next = even_head
+        return head
